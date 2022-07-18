61d901bd802ce5f57b89fc3b43f2d552dfa141d3
Artem Kireev <al-mysri@yandex.ru>
2022-07-18 14:49:36 +0300

    feat: methoda

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.headers.jwt;
 
   if (!token) {
     throw new UnauthorizedError('Необходима авторизация');
