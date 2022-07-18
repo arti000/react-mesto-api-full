6d24a894fefc327f0d267e51686924344f34e504
Artem Kireev <al-mysri@yandex.ru>
2022-07-18 14:08:39 +0300

    feat: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,28 +1,28 @@
 const allowedCors = [
-  'https://api.mesto.rt.front.nomoredomains.sbs',
-  'https://mesto.rt.front.nomoredomains.sbs',
+  'mesto.rt.front.nomoredomains.sbs',
+  'localhost:3000',
   'http://localhost:3000',
+  'http://mesto.rt.front.nomoredomains.sbs',
+  'https://mesto.rt.front.nomoredomains.sbs',
   'https://localhost:3000',
 ];
 
-const cors = (req, res, next) => {
+module.exports = ((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    return res.end();
+    res.status(200).send();
+    return;
   }
-
-  return next();
-};
-
-module.exports = cors;
+  next();
+});
