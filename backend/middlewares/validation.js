e3b91d84bff4007586b6bbc1a614995fae5246cf
Artem Kireev <al-mysri@yandex.ru>
2022-07-22 02:09:40 +0300

    fix: remove required

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -28,7 +28,7 @@ const validateUser = celebrate({
 const validateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().custom(validateUrl).required(),
+    link: Joi.string().custom(validateUrl),
   }),
 });
 
